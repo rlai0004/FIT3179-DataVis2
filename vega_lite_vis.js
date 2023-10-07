007a035d206c40119075801c799bf74a2c840add
Ryan Dylan Lai <rlai0004@student.monash.edu>
2023-10-08 03:42:05 +1100

    Week 10 HW

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,12 @@
 // var vg_1 = "daily_cost_bar_chart.vg.json";
 var vg_1 = "week9mapv2.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function (result) {
+vegaEmbed("#choropleth_map", vg_1).then(function (result) {
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "week10bubble.vg.json";
+vegaEmbed("#bubble_chart", vg_2).then(function (result) {
     // Access the Vega view instance 
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
