6914be7896d2f582cb129c39a78c548f23942e74
Ryan Dylan Lai <rlai0004@student.monash.edu>
2023-10-09 16:04:00 +1100

    Week 11 Critique
    -Updated boxplot, bubble chart, choropleth
    - Added bar chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,12 +1,16 @@
 // var vg_1 = "daily_cost_bar_chart.vg.json";
 var vg_1 = "week9mapv2.vg.json";
 vegaEmbed("#choropleth_map", vg_1).then(function (result) {
-    // Access the Vega view instance 
-    // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "week10bubble.vg.json";
 vegaEmbed("#bubble_chart", vg_2).then(function (result) {
-    // Access the Vega view instance 
-    // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "boxplot.vg.json";
+vegaEmbed("#boxplot", vg_3).then(function (result) {
+}).catch(console.error);
+
+var vg_4 = "stackedbarchart.vg.json";
+vegaEmbed("#stacked_bar_chart", vg_4).then(function (result) {
 }).catch(console.error);
